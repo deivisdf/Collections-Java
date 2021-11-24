49fc8e0b3618d9cc5fd32dd7ac22f2fab2fc1339
Deivis <deivisbsb@gmail.com>
2021-11-23 21:47:08 -0300

    Implementações das interfaces Comparator e Comparable

---
+++ src/br/com/deivis/list/ComparatorNomeCorIdade.java
@@ -0,0 +1,13 @@
+package br.com.deivis.list;
+
+import java.util.Comparator;
+
+public class ComparatorNomeCorIdade  implements Comparator<Gato> {
+    @Override
+    public int compare(Gato o1, Gato o2) {
+
+        // tentar fazer usando ternario
+
+        return 0;
+    }
+}
